0ea8ea93a7108b2c90f97f6d028196f06229d576
unw9527 <1041593558@qq.com>
2022-08-12 21:00:12 +0800

    fix: chromedriver config

--- check_in.py
+++ check_in.py
@@ -12,7 +12,14 @@ class Checkin():
         self.url = url
     
     def tnt_check(self):
-        driver = webdriver.Chrome()
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        chromedriver = "/usr/bin/chromedriver"
+        os.environ["webdriver.chrome.driver"] = chromedriver
+        driver = webdriver.Chrome(chrome_options=chrome_options,executable_path=chromedriver)
         driver.get(self.url)
         
         # login
