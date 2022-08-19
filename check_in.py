e469d06d9e902dd88b5e0154fe5dd93b42c5eda3
unw9527 <1041593558@qq.com>
2022-08-19 14:38:46 +0800

    rm: timeout exception

--- check_in.py
+++ check_in.py
@@ -32,12 +32,9 @@ class Checkin():
         WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="tos_notify"]/div/div/div/div/div/button'))).click()
         
         # click check in button
-        try:
-            WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'checkin'))).click()
-            time.sleep(10)
-            print('Check in successfully!')
-        except TimeoutException:
-            print('Has already checked in today!')
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, 'checkin'))).click()
+        time.sleep(10)
+        print('Check in successfully!')
         driver.quit()
 
 if __name__ == '__main__':
